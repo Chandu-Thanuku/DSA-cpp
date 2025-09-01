effd8c2dde6d4b3818dd159d9e6783da0815fd9a
Chandu <chanduthanuku24@gmail.com>
2025-09-01 22:19:30 +0530

    Create pattern.cpp

---
+++ Pattern Printing/ Inverted Right Pyramid/pattern.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+
+    void printTriangle(int n) {
+      for(int i=1;i<=n;i++){
+          for(int j=n;j>=i;j--){
+              cout<<"* ";
+          }
+          cout<<endl;
+      }
+    }
+};
